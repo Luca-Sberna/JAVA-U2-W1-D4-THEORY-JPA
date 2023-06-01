440a99c87255cc5ccae0f09ab6f3f0811af2c56a
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-06-01 10:56:15 +0200

    add

---
+++ src/main/java/U2W1D4THEORYJPA/U2W1D4TheoryJpaApplication.java
@@ -0,0 +1,13 @@
+package U2W1D4THEORYJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W1D4TheoryJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W1D4TheoryJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/U2W1D4THEORYJPA/U2W1D4TheoryJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package U2W1D4THEORYJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W1D4TheoryJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
